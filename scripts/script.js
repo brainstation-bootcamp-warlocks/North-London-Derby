0947b8d20912441c664ee044882de1c95e8d78fb
unknown <khalid.zein1@gmail.com>
2022-04-25 19:57:02 -0400

    Third Commit: Fixed the display of the character after generating the quote in JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,50 @@
-let quoteGenerator = ()=>{
-    let quoteSection = document.getElementById( 'quoteMain' );
-    quoteSection.classList.add( 'quoteSection' );
-
-    
+const simpsonsQuoteAPI = 'https://thesimpsonsquoteapi.glitch.me/quotes';
+const quoteGenerator = ()=>{
+    axios.get(simpsonsQuoteAPI)
+        .then((response)=>{
+            let data = response.data[0];
+            const quoteDisplay = (event) => {
+                const quotesAPI = document.querySelector( '.quotes__text' );
+                quotesAPI.innerText = data.quote;
+                const characterAPI = document.querySelector( '.characters__image');
+                characterAPI.src = data.image;
+                characterAPI.style.display = 'none';
+                event.preventDefault();
+            }
+            console.log(data);
+            return quoteDisplay();
+        })
+        .catch((e) =>{
+            console.log(e);
+        })
 }
+const showCharacter = (event) => {
+    event.preventDefault();
+    const characterClass = document.querySelector( '.characters__image');
+    characterClass.style.display = 'block';
+}
+// const characterGenerator = ()=>{
+//     axios.get(simpsonsQuoteAPI)
+//     .then((response)=>{
+//         let data = response.data[0];
+//         if (quoteGenerator() === data.name){
+//             const imageAPI = document.querySelector( '.characters__image' );
+//             imageAPI.src = data.image;
+//         }
+//         //console.log(data);
+//     })
+//     .catch((e) =>{
+//         console.log(e);
+//     })
+// }
+// const quoteDisplay = (event) => {
+//     quoteGenerator();
+// }
+// const characterDisplay = (event)=> {
+//     characterGenerator();
+// }
+//const returnValues = quoteGenerator();
+const quotesButton = document.querySelector('.quotes__button');
+quotesButton.addEventListener('click', quoteGenerator);
+const charactersButton = document.querySelector( '.characters__button' );
+charactersButton.addEventListener( 'click', showCharacter );
