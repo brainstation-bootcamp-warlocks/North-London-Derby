6f827cf85fb617fd5b47402a184787fe169820ef
unknown <khalid.zein1@gmail.com>
2022-04-28 19:29:15 -0400

    Fourth Commit: Added classes, variable, and mixins to the sass styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,21 @@
 const simpsonsQuoteAPI = 'https://thesimpsonsquoteapi.glitch.me/quotes';
-const quoteGenerator = ()=>{
+
+
+const quoteGenerator = () => {
     axios.get(simpsonsQuoteAPI)
-        .then((response)=>{
+        .then((response) => {
             let data = response.data[0];
+
             const quoteDisplay = (event) => {
-                const quotesAPI = document.querySelector( '.quotes__text' );
+                const quotesAPI = document.querySelector('.tv__text');
                 quotesAPI.innerText = data.quote;
-                const characterAPI = document.querySelector( '.characters__image');
+                quotesAPI.style.display = 'block';
+                
+                const characterAPI = document.querySelector('.tv__characters');
                 characterAPI.src = data.image;
                 characterAPI.style.display = 'none';
-                event.preventDefault();
             }
+
             console.log(data);
             return quoteDisplay();
         })
@@ -18,33 +23,17 @@ const quoteGenerator = ()=>{
             console.log(e);
         })
 }
+
 const showCharacter = (event) => {
     event.preventDefault();
-    const characterClass = document.querySelector( '.characters__image');
+    const characterClass = document.querySelector('.tv__characters');
     characterClass.style.display = 'block';
+    const quotesAPI = document.querySelector('.tv__text');
+    quotesAPI.style.display = 'none';
 }
-// const characterGenerator = ()=>{
-//     axios.get(simpsonsQuoteAPI)
-//     .then((response)=>{
-//         let data = response.data[0];
-//         if (quoteGenerator() === data.name){
-//             const imageAPI = document.querySelector( '.characters__image' );
-//             imageAPI.src = data.image;
-//         }
-//         //console.log(data);
-//     })
-//     .catch((e) =>{
-//         console.log(e);
-//     })
-// }
-// const quoteDisplay = (event) => {
-//     quoteGenerator();
-// }
-// const characterDisplay = (event)=> {
-//     characterGenerator();
-// }
-//const returnValues = quoteGenerator();
-const quotesButton = document.querySelector('.quotes__button');
+
+const quotesButton = document.querySelector('.tv__quote-button');
 quotesButton.addEventListener('click', quoteGenerator);
-const charactersButton = document.querySelector( '.characters__button' );
+
+const charactersButton = document.querySelector('.tv__characters-button');
 charactersButton.addEventListener( 'click', showCharacter );
